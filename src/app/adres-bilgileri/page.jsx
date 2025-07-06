fab87bcb1ce1d728854687d3938b0b0dd85be6cb
gokayybaz <gokaybaz2000@outlook.com>
2025-07-06 23:47:40 +0300

    Address Selector Checkboxes Changed To Radio Button

--- src/app/adres-bilgileri/page.jsx
+++ src/app/adres-bilgileri/page.jsx
@@ -7,10 +7,14 @@ import { ChevronRight } from "lucide-react"
 import { Plus } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useBasket } from "@/contexts/BasketContext"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { useState } from "react"
 
 const page = () => {
     const router = useRouter()
     const { totalProductsPrice } = useBasket()
+    const [isOrderAddressEqualsInvoiceAddress, setIsOrderAddressEqualsInvoiceAddress] = useState(false)
+    const [selectedCargoCompany, setSelectedCargoCompany] = useState('none')
     return (
         <div className="px-60 flex py-12 gap-12">
             <div className="flex flex-col gap-8">
@@ -22,25 +26,37 @@ const page = () => {
                     <CardHeader>
                         <h2 className="font-semibold">Kargo Firması Seçimi</h2>
                     </CardHeader>
-                    <CardContent className="flex items-center justify-between">
-                        <div>
-                            <span className="flex items-center gap-2">
-                                <Checkbox />
-                                <h3>Aras Kargo</h3>
+                    <CardContent className="flex w-full">
+
+                        <RadioGroup className="flex w-full items-center justify-between" defaultValue={"aras-kargo"}>
+                            <span className="flex cursor-pointer items-center gap-2">
+                                <RadioGroupItem value="Aras Kargo" id="aras-kargo" />
+                                <Label className="cursor-pointer" htmlFor="aras-kargo">Aras Kargo</Label>
                             </span>
-                        </div>
-                        <div>
-                            <span className="flex items-center gap-2">
-                                <Checkbox />
-                                <h3>Yurtiçi Kargo</h3>
+
+
+                            <span className="flex cursor-pointer items-center gap-2">
+                                <RadioGroupItem value="Yurtiçi Kargo" id="yurtici-kargo" />
+                                <Label className="cursor-pointer" htmlFor="yurtici-kargo">Yurtiçi Kargo</Label>
                             </span>
-                        </div>
-                        <div>
-                            <span className="flex items-center gap-2">
-                                <Checkbox />
-                                <h3>DHL Kargo</h3>
+
+
+                            <span className="flex cursor-pointer  items-center gap-2">
+                                <RadioGroupItem value="DHL Kargo" id="dhl-kargo" />
+                                <Label className="cursor-pointer" htmlFor="dhl-kargo">DHL Kargo</Label>
                             </span>
-                        </div>
+
+                            <span className="flex cursor-pointer items-center gap-2">
+                                <RadioGroupItem value="Kolay Gelsin" id="kolay-gelsin" />
+                                <Label className="cursor-pointer" htmlFor="kolay-gelsin">Kolay Gelsin</Label>
+                            </span>
+
+                            <span className="flex cursor-pointer items-center gap-2">
+                                <RadioGroupItem value="HepsiJET" id="hepsi-jet" />
+                                <Label className="cursor-pointer" htmlFor="hepsi-jet">HepsiJET</Label>
+                            </span>
+                        </RadioGroup>
+
                         {/* <Card>
                             <CardHeader>
                                 <span className="flex items-center gap-2">
@@ -49,98 +65,93 @@ const page = () => {
                                 </span>
                             </CardHeader>
                         </Card> */}
-                        <div>
-                            <span className="flex items-center gap-2">
-                                <Checkbox />
-                                <h3>Kolay Gelsin</h3>
-                            </span>
-                        </div>
-                        <div>
-                            <span className="flex items-center gap-2">
-                                <Checkbox />
-                                <h3>HepsiJET</h3>
-                            </span>
-                        </div>
+
                     </CardContent>
                 </Card>
                 <Card className="h-56" >
                     <CardHeader>
                         <h2 className="font-semibold">Gönderi Adresi Seçimi</h2>
                     </CardHeader>
-                    <CardContent className="flex items-center gap-8 h-4/6">
-                        <Card className="flex w-2/12  items-center justify-center h-full">
-                            <CardHeader>
-                                <span className="flex flex-col gap-2 items-center justify-between">
-                                    <Plus size={30} />
-                                    <p className="text-center">Yeni Adres Ekle</p>
-                                </span>
-                            </CardHeader>
-                        </Card>
-                        <Card className="w-5/12 h-full">
-                            <CardHeader>
-                                <span className="flex  items-center gap-2">
-                                    <Checkbox className="h-4 w-4" id="address-title" />
-                                    <Label className="text-lg" htmlFor="address-title">Ev</Label>
-                                </span>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="line-clamp-1 select-none">Prof. Dr. Yusuf Halaçoğlu Cad. 24 Kasım Evleri No:B3 Kat:4 Daire:8</p>
-                            </CardContent>
-                        </Card>
-                        <Card className="w-5/12 h-full">
-                            <CardHeader>
-                                <span className="flex  items-center gap-2">
-                                    <Checkbox className="h-4 w-4" id="address-title" />
-                                    <Label className="text-lg" htmlFor="address-title">Binboğa Bal</Label>
-                                </span>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="line-clamp-1 select-none">Varsaklar Mah. Adana Cad. No:219/A Kozan/ADANA</p>
-                            </CardContent>
-                        </Card>
+                    <CardContent >
+                        <RadioGroup className="flex items-center gap-8 h-4/6">
+                            <Card className="flex w-2/12  items-center justify-center h-full">
+                                <CardHeader>
+                                    <span className="flex flex-col gap-2 items-center justify-between">
+                                        <Plus size={30} />
+                                        <p className="text-center">Yeni Adres Ekle</p>
+                                    </span>
+                                </CardHeader>
+                            </Card>
+                            <Card className="w-5/12 h-full">
+                                <CardHeader>
+                                    <span className="flex  items-center gap-2">
+                                        <RadioGroupItem className="h-4 w-4" value="Ev Adresim" id="address-title" />
+                                        <Label className="cursor-pointer text-lg" htmlFor="address-title">Ev</Label>
+                                    </span>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="line-clamp-1 select-none">Prof. Dr. Yusuf Halaçoğlu Cad. 24 Kasım Evleri No:B3 Kat:4 Daire:8</p>
+                                </CardContent>
+                            </Card>
+                            <Card className="w-5/12 h-full">
+                                <CardHeader>
+                                    <span className="flex  items-center gap-2">
+                                        <RadioGroupItem className="h-4 w-4" value="İş Adresim" id="address2-title" />
+                                        <Label className="cursor-pointer text-lg" htmlFor="address2-title">Binboğa Bal</Label>
+                                    </span>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="line-clamp-1 select-none">Varsaklar Mah. Adana Cad. No:219/A Kozan/ADANA</p>
+                                </CardContent>
+                            </Card>
+                        </RadioGroup>
                     </CardContent>
                 </Card>
                 <div className="flex my-2 items-center gap-2">
-                    <Checkbox className="w-5 h-5" id="invoice-address" />
-                    <Label className="text-lg" htmlFor="invoice-address">Gönderim Adresim ile Fatura Adresim Aynı</Label>
+                    <Checkbox onCheckedChange={(checked) => setIsOrderAddressEqualsInvoiceAddress(!isOrderAddressEqualsInvoiceAddress)} value={isOrderAddressEqualsInvoiceAddress} className="w-5 h-5" id="invoice-address" />
+                    <Label className="cursor-pointer text-lg" htmlFor="invoice-address">Gönderim Adresim ile Fatura Adresim Aynı</Label>
                 </div>
-                <Card className="h-56" >
-                    <CardHeader>
-                        <h2 className="font-semibold">Fatura Adresi Seçimi</h2>
-                    </CardHeader>
-                    <CardContent className="flex items-center gap-8 h-4/6">
-                        <Card className="flex w-2/12  items-center justify-center h-full">
-                            <CardHeader>
-                                <span className="flex flex-col gap-2 items-center justify-between">
-                                    <Plus size={30} />
-                                    <p className="text-center">Yeni Adres Ekle</p>
-                                </span>
-                            </CardHeader>
-                        </Card>
-                        <Card className="w-5/12 h-full">
-                            <CardHeader>
-                                <span className="flex  items-center gap-2">
-                                    <Checkbox className="h-4 w-4" id="address-title" />
-                                    <Label className="text-lg" htmlFor="address-title">Ev</Label>
-                                </span>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="line-clamp-1 select-none">Prof. Dr. Yusuf Halaçoğlu Cad. 24 Kasım Evleri No:B3 Kat:4 Daire:8</p>
-                            </CardContent>
-                        </Card>
-                        <Card className="w-5/12 h-full">
-                            <CardHeader>
-                                <span className="flex  items-center gap-2">
-                                    <Checkbox className="h-4 w-4" id="address-title" />
-                                    <Label className="text-lg" htmlFor="address-title">Binboğa Bal</Label>
-                                </span>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="line-clamp-1 select-none">Varsaklar Mah. Adana Cad. No:219/A Kozan/ADANA</p>
-                            </CardContent>
-                        </Card>
-                    </CardContent>
-                </Card>
+                {
+                    isOrderAddressEqualsInvoiceAddress != true ? (<Card className="h-56" >
+                        <CardHeader>
+                            <h2 className="font-semibold">Fatura Adresi Seçimi</h2>
+                        </CardHeader>
+                        <CardContent >
+                            <RadioGroup className="flex items-center gap-8 h-4/6">
+                                <Card className="flex w-2/12  items-center justify-center h-full">
+                                    <CardHeader>
+                                        <span className="flex flex-col gap-2 items-center justify-between">
+                                            <Plus size={30} />
+                                            <p className="text-center">Yeni Adres Ekle</p>
+                                        </span>
+                                    </CardHeader>
+                                </Card>
+                                <Card className="w-5/12 h-full">
+                                    <CardHeader>
+                                        <span className="flex  items-center gap-2">
+                                            <RadioGroupItem value="Ev Adresim" className="h-4 w-4" id="invoice-address-title" />
+                                            <Label className="cursor-pointer text-lg" htmlFor="invoice-address-title">Ev</Label>
+                                        </span>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <p className="line-clamp-1 select-none">Prof. Dr. Yusuf Halaçoğlu Cad. 24 Kasım Evleri No:B3 Kat:4 Daire:8</p>
+                                    </CardContent>
+                                </Card>
+                                <Card className="w-5/12 h-full">
+                                    <CardHeader>
+                                        <span className="flex  items-center gap-2">
+                                            <RadioGroupItem value="İş Adresim" className="h-4 w-4" id="invoice-address2-title" />
+                                            <Label className="text-lg cursor-pointer" htmlFor="invoice-address2-title">Binboğa Bal</Label>
+                                        </span>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <p className="line-clamp-1 select-none">Varsaklar Mah. Adana Cad. No:219/A Kozan/ADANA</p>
+                                    </CardContent>
+                                </Card>
+                            </RadioGroup>
+                        </CardContent>
+                    </Card>) : null
+                }
 
             </div>
             <div className="w-2/6 flex flex-col gap-10">
